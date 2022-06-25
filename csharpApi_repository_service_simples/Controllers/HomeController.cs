059ed069e6c14db2f139cf0e2d2cb9cf891d6b34
jiraspiom <jiraspiom@yahoo.com.br>
2022-06-25 08:30:22 -0300

    Adicionar arquivos de projeto.

---
+++ csharpApi_repository_service_simples/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace csharpApi_repository_service_simples.Controllers
+{
+    [ApiController]
+    [Route("V1")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
